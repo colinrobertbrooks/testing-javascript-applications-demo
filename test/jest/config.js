b7daf7da77553b719edcebd2c058ff1e8c97e8ee
colinrcummings <colinrcummings@gmail.com>
2018-10-25 14:48:52 -0400

    code coverage

--- test/jest/config.js
+++ test/jest/config.js
@@ -1,5 +1,8 @@
 module.exports = {
   rootDir: '../../',
   roots: ['test/jest/specs'],
-  moduleDirectories: ['node_modules', 'test/jest/helpers']
+  moduleDirectories: ['node_modules', 'test/jest/helpers'],
+  collectCoverageFrom: ['app/**/*.js'],
+  coverageDirectory: 'test/jest/coverage',
+  coverageReporters: ['lcov', 'text-summary']
 };
