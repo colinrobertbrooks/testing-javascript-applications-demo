172b97ebbbfec188ab9794991ef9e3871f8ea1da
Colin Cummings <colin_cummings@brown.edu>
2019-03-11 15:12:08 -0400

    update dependencies

--- test/jest/config.js
+++ test/jest/config.js
@@ -2,7 +2,7 @@ module.exports = {
   rootDir: '../../',
   roots: ['test/jest/specs'],
   moduleDirectories: ['node_modules', 'test/jest/helpers'],
-  setupTestFrameworkScriptFile: 'test/jest/setup.js',
+  setupFilesAfterEnv: ['test/jest/setup.js'],
   collectCoverageFrom: ['app/**/*.js'],
   coverageDirectory: 'test/jest/coverage',
   coverageReporters: ['lcov', 'text-summary']
